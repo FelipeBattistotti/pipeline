410ba269fc3bfffaf370d56fd5c7dac3907d5451
FelipeBattistotti <felipe.battistotti@gmail.com>
2023-09-28 09:48:04 -0300

    [UPDATE] react-ui-kanban

--- src/components/KanbanBoard/index.tsx
+++ src/components/KanbanBoard/index.tsx
@@ -6,50 +6,137 @@ import Board from 'react-ui-kanban'
 
 const KanbanBoard = () => {
   const [data, setData] = useState({
-    lanes: [
+    "lanes": [
       {
-        id: 'lane1',
-        title: 'To do',
-        cards: [
+        "id": "PLANNED",
+        "title": "Planned Tasks",
+        "label": "20/70",
+        "style": {"width": 280},
+        "cards": [
           {
-            id: 'card1',
-            title: 'Task 1',
-            description: 'This is the first task',
-            label: '5 mins',
+            "id": "Milk",
+            "title": "Buy milk",
+            "label": "15 mins",
+            "description": "2 Gallons of milk at the Deli store"
           },
           {
-            id: 'card2',
-            title: 'Task 2',
-            description: 'This is the second task',
-            label: '10 mins',
+            "id": "Plan2",
+            "title": "Dispose Garbage",
+            "label": "10 mins",
+            "description": "Sort out recyclable and waste as needed"
           },
-        ],
+          {
+            "id": "Plan3",
+            "title": "Write Blog",
+            "label": "30 mins",
+            "description": "Can AI make memes?"
+          },
+          {
+            "id": "Plan4",
+            "title": "Pay Rent",
+            "label": "5 mins",
+            "description": "Transfer to bank account"
+          }
+        ]
       },
       {
-        id: 'lane2',
-        title: 'In progress',
-        cards: [
+        "id": "WIP",
+        "title": "Work In Progress",
+        "label": "10/20",
+        "style": {"width": 280},
+        "cards": [
           {
-            id: 'card3',
-            title: 'Task 3',
-            description: 'This is the third task',
-            label: '15 mins',
-          },
-        ],
+            "id": "Wip1",
+            "title": "Clean House",
+            "label": "30 mins",
+            "description": "Soap wash and polish floor. Polish windows and doors. Scrap all broken glasses"
+          }
+        ]
       },
       {
-        id: 'lane3',
-        title: 'Done',
-        cards: [
-          {
-            id: 'card4',
-            title: 'Task 4',
-            description: 'This is the fourth task',
-            label: '20 mins',
-          },
-        ],
+        "id": "BLOCKED",
+        "title": "Blocked",
+        "label": "0/0",
+        "style": {"width": 280},
+        "cards": []
       },
-    ],
+      {
+        "id": "COMPLETED",
+        "title": "Completed",
+        "style": {"width": 280},
+        "label": "2/5",
+        "cards": [
+          {
+            "id": "Completed1",
+            "title": "Practice Meditation",
+            "label": "15 mins",
+            "description": "Use Headspace app"
+          },
+          {
+            "id": "Completed2",
+            "title": "Maintain Daily Journal",
+            "label": "15 mins",
+            "description": "Use Spreadsheet for now"
+          }
+        ]
+      },
+      {
+        "id": "REPEAT",
+        "title": "Repeat",
+        "style": {"width": 280},
+        "label": "1/1",
+        "cards": [
+          {
+            "id": "Repeat1",
+            "title": "Morning Jog",
+            "label": "30 mins",
+            "description": "Track using fitbit"
+          }
+        ]
+      },
+      {
+        "id": "ARCHIVED",
+        "title": "Archived",
+        "style": {"width": 280},
+        "label": "1/1",
+        "cards": [
+          {
+            "id": "Archived1",
+            "title": "Go Trekking",
+            "label": "300 mins",
+            "description": "Completed 10km on cycle"
+          }
+        ]
+      },
+      {
+        "id": "ARCHIVED2",
+        "title": "Archived2",
+        "style": {"width": 280},
+        "label": "1/1",
+        "cards": [
+          {
+            "id": "Archived2",
+            "title": "Go Jogging",
+            "label": "300 mins",
+            "description": "Completed 10km on cycle"
+          }
+        ]
+      },
+      {
+        "id": "ARCHIVED3",
+        "title": "Archived3",
+        "style": {"width": 280},
+        "label": "1/1",
+        "cards": [
+          {
+            "id": "Archived3",
+            "title": "Go Cycling",
+            "label": "300 mins",
+            "description": "Completed 10km on cycle"
+          }
+        ]
+      }
+    ]
   });
 
   const handleDragEnd = (cardId: string, sourceLaneId: string, targetLaneId: string) => {
@@ -79,7 +166,8 @@ const KanbanBoard = () => {
     });
   };
 
-  return <Board data={data} draggable handleDragEnd={handleDragEnd} />;
+  // return <Board data={data} draggable handleDragEnd={handleDragEnd} />;
+  return <Board data={data} />
 };
 
 export default KanbanBoard;
